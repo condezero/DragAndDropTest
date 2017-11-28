cc2c3f255f59bf0e3b8c13de48a3ef75ed1cc24e
Carlos Lisandra <condezero.monalisa@gmail.com>
2017-11-28 01:12:01 +0100

    test

---
+++ aceler/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Vector3 oldVel;
+    private Vector3 screenPoint;
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+    }
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 currentPos = Camera.main.ScreenToWorldPoint(currentScreenPoint);
+        transform.position = currentPos;
+    }
+    void Start()
+    {
+        if (myRigidbody.tag == "ball")
+        {
+            myRigidbody = GetComponent<Rigidbody>();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        oldVel = myRigidbody.velocity;
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        ContactPoint cp = c.contacts[0];
+        // calculate with addition of normal vector
+        // myRigidbody.velocity = oldVel + cp.normal*2.0f*oldVel.magnitude;
+
+        // calculate with Vector3.Reflect
+        myRigidbody.velocity = Vector3.Reflect(oldVel, cp.normal);
+
+        // bumper effect to speed up ball
+        myRigidbody.velocity += cp.normal * 2.0f;
+        foreach (ContactPoint contact in c.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+    }
+}
---
+++ aceler/Assets/Scripts/DragObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(BoxCollider2D))]
+public class DragObject : MonoBehaviour {
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private Camera theCam;
+    private bool dragging;
+    private Rigidbody2D rBody;
+    public float Speed;
+    void OnMouseDown()
+    {
+        if (gameObject.tag == "ball")
+        {
+            Debug.Log("suelta");
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+            
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+       
+    }
+
+    void OnMouseUp()
+    {
+            float moveHorizontal = Input.GetAxis("Mouse X");
+            float moveVertical = Input.GetAxis("Mouse Y");
+
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical);
+
+            rBody.AddForce(movement , ForceMode2D.Impulse);
+    }
+    //void FixedUpdate(float h, float v)
+    //{
+    //    float moveHorizontal = Input.GetAxis("Mouse X");
+    //    float moveVertical = Input.GetAxis("Mouse Y");
+
+    //    Vector3 movement = new Vector3(moveHorizontal, moveVertical);
+
+    //    rBody.AddForce(movement , ForceMode2D.Impulse);
+    //}
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+}
